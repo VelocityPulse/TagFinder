960f76203943ff665ccc6718171668b84d2ebc70
Velocitypulse <velocitypulse@outlook.fr>
2020-05-15 14:02:14 +0200

    [INIT]

---
+++ app/src/main/java/com/cpulse/tagfinder/BrowserActivity.java
@@ -0,0 +1,23 @@
+package com.cpulse.tagfinder;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class BrowserActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+    }
+
+
+}
